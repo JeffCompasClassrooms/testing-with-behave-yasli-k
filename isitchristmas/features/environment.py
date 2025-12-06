4074c3a6a7e971da922ea36cf8e0ad33c442e1fd
YasliK <114773825+yasli-k@users.noreply.github.com>
2025-12-06 12:58:44 -0700

    fixed

--- isitchristmas/features/environment.py
+++ isitchristmas/features/environment.py
@@ -2,7 +2,7 @@ import behave_webdriver
 from behave_webdriver.steps import *
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome()
+    context.behave_driver = behave_webdriver.Chrome.headless()
 
 def after_all(context):
     context.behave_driver.quit()
